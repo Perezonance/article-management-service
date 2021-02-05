9db6a657269f209126958e737a3945e0e344a78a
Alex Perez <manuel.perez@accesscfa.com>
2021-02-05 00:29:38 -0500

    assign handlers for each method/request

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,22 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Perezonance/article-management-service/internal/controllers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/articles", articleHandler)
-	r.HandleFunc("/articles/{articleID}", articleByIDHandler)
+	c := controllers.NewController()
+
+	r.HandleFunc("/articles", c.GetArticles).Methods(http.MethodGet)
+	r.HandleFunc("/articles", c.PostArticle).Methods(http.MethodPost)
+
+	r.HandleFunc("/articles/{articleID}", c.GetArticleByID).Methods(http.MethodGet)
+	r.HandleFunc("/articles/{articleID}", c.UpdateArticleByID).Methods(http.MethodPut)
+	r.HandleFunc("/articles/{articleID}", c.DeleteArticleByID).Methods(http.MethodDelete)
+
 	r.HandleFunc("/articles/{userID}", articleByUserIDHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", r))
