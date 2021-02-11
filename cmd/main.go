1fd4e0029333e89d4c806ce845f814ef46a5e344
Alex Perez <manuel.perez@accesscfa.com>
2021-02-10 23:56:40 -0500

    accident code

--- cmd/main.go
+++ cmd/main.go
@@ -15,8 +15,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func start()
-
 func main() {
 	var wait time.Duration
 	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
